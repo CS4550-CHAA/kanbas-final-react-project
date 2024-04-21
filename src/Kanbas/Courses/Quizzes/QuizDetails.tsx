3716139a76b4ba7ffd204f492dbffe86718537ca
Adalia Lee <adalialee@gmail.com>
2024-04-21 16:00:41 -0700

    styled more of quiz details

--- src/Kanbas/Courses/Quizzes/QuizDetails.tsx
+++ src/Kanbas/Courses/Quizzes/QuizDetails.tsx
@@ -65,12 +65,13 @@ function QuizDetails() {
       </div>
       <hr />
       <h3>{quiz?.title}</h3>
-      <div style={{ display: "flex", flexDirection: "row" }}>
+      <div style={{ display: "flex", flexDirection: "row", paddingTop: "50px", paddingBottom: "50px" }}>
         <div
           style={{
             display: "flex",
             flexDirection: "column",
-            alignItems: "right",
+            alignItems: "flex-end",
+            paddingRight: "20px"
           }}
         >
           <label>Quiz Type</label>
@@ -99,33 +100,34 @@ function QuizDetails() {
           <label>{quiz?.lockQuestionsAfterAnswering}</label>
         </div>
       </div>
+      
       <div
         style={{
           display: "flex",
           flexDirection: "row",
           justifyContent: "space-between",
-          width: "80%",
         }}
       >
-        <h6>Due</h6>
-        <h6>For</h6>
-        <h6>Available from</h6>
-        <h6>Until</h6>
-      </div>
-      <hr />
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "space-between",
-          width: "80%",
-        }}
-      >
-        {/* TODO fix format of dates */}
-        {String(quiz?.dueDate)}
-        <label>Everyone</label>
-        {String(quiz?.availableDate)}
-        {String(quiz?.untilDate)}
+        <div style={{display: "flex", flexDirection: "column", width: "100%"}}>
+          <h6>Due</h6>
+          <hr style={{ width: "100%" }}/>
+          {String(quiz?.dueDate)}
+        </div>
+        <div style={{display: "flex", flexDirection: "column", width: "100%"}}>
+          <h6>For</h6>
+          <hr style={{ width: "100%" }}/>
+          <label>Everyone</label>
+        </div>
+        <div style={{display: "flex", flexDirection: "column", width: "100%"}}>
+          <h6>Available from</h6>
+          <hr style={{ width: "100%" }}/>
+          {String(quiz?.availableDate)}
+        </div>
+        <div style={{display: "flex", flexDirection: "column"}}>
+          <h6 style={{paddingRight: "50px"}}>Until</h6>
+          <hr style={{ width: "100%" }}/>
+          <label style={{paddingRight: "50px"}}>{String(quiz?.untilDate)}</label>
+        </div>
       </div>
       <hr />
     </div>
